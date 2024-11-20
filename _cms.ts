799efa917e6cbd013ad9931a6aa8b9783b26ad44
Sergey Mochalov <proweb@users.noreply.github.com>
2024-11-20 22:47:25 +0300

    cms config update

--- _cms.ts
+++ _cms.ts
@@ -1,21 +1,59 @@
 import lumeCMS from "lume/cms/mod.ts";
 
-
+// Basic config
 const config = {
   site: {
-    name: "Static CMS",
-    description: "Here I can edit the content of my blog",
+    name: "Статические сайты",
+    description: "админка для управления сайтом",
   },
   log: {
     filename: "errors.log",
   },
 };
 const cms = lumeCMS(config);
+// Collection for blog posts
+cms.collection({
+  name: "posts",
+  description: "Записи в блоге",
+  store: "src:posts/*.md",
+  fields: [
+    {
+      name: "title",
+      type: "text",
+      label: "Заголовок",
+      attributes: {
+        required: true,
+        maxlength: 100,
+      },
+    },
+    {
+      name: "date",
+      type: "date",
+    },
+    {
+      name: "draft",
+      type: "checkbox",
+      label: "Черновик",
+      view: "editor",
+    },
+    {
+      name: "content",
+      type: "markdown",
+      label: "Текст статьи",
+      attributes: {
+        required: true,
+      },
+    },
+  ],
+});
+// Collection for pages
+cms.collection({
+    name: "pages", 
+    description: "Страницы сайта",
+    store: "src:pages/*.md", 
+    fields:["title: text", "content: markdown"]
+});
 
-cms.collection(
-    "posts", 
-    "src:posts/*.md", 
-    ["title: text", "content: markdown"]
-);
+cms.upload("images", "src:images");
 
 export default cms;
