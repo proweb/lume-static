4f6b726f3c9c95e2646394d49b2d78f804d3c7a9
Sergey Mochalov <svmochalov@kpfu.ru>
2024-11-20 18:00:38 +0300

    CMS reconfig

--- _cms.ts
+++ _cms.ts
@@ -1,5 +1,21 @@
 import lumeCMS from "lume/cms/mod.ts";
 
-const cms = lumeCMS();
+
+const config = {
+  site: {
+    name: "Static CMS",
+    description: "Here I can edit the content of my blog",
+  },
+  log: {
+    filename: "errors.log",
+  },
+};
+const cms = lumeCMS(config);
+
+cms.collection(
+    "posts", 
+    "src:posts/*.md", 
+    ["title: text", "content: markdown"]
+);
 
 export default cms;
