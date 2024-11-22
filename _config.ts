435a2b617e6d1c67c657b5647dc8898bcc2e3364
Sergey Mochalov <proweb@users.noreply.github.com>
2024-11-22 09:17:52 +0000

    Base path plugin

--- _config.ts
+++ _config.ts
@@ -1,14 +1,18 @@
 import lume from "lume/mod.ts";
+import basePath from "lume/plugins/base_path.ts";
 
 const config = {
   site: {
     name: "Статические сайты",
     description: "админка для управления сайтом",
   },
+  location: new URL("https://proweb.github.io/lume-static/"),
   src: "./src",
 };
 
 const site = lume(config);
+
+site.use(basePath());
 site.copy("images");
 
 export default site;
